c1b0404b9eac5170a04e30edbfd7429b452f4e83
Andrew R Emery <hello@andrewremery.com>
2022-01-16 11:08:44 -0500

    :sparkles: Add event resize event listener to desktop hover so it's responsive not just on load

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
     $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
         if (!$(this).next().hasClass('show')) {
             $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
-            
+
         }
         var $subMenu = $(this).next('.dropdown-menu');
         $subMenu.toggleClass('show');
-       
+
         $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
             $('.dropdown-submenu .show').removeClass('show');
-          
+
         });
 
         return false;
@@ -26,34 +26,41 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
-    // Make sub menu trigger on mouse in/out on desktop
-       if (window.innerWidth > 768) { //if bigger then tablet
+    function dropdownOnHover() {
+        // Make sub menu trigger on mouse in/out on desktop
+        if (window.innerWidth > 768) { //if bigger then tablet
 
-        document.querySelectorAll('.navbar .nav-item').forEach(function (everyitem) {
+            document.querySelectorAll('.navbar .nav-item').forEach(function (everyitem) {
 
-            everyitem.addEventListener('mouseover', function (e) {
+                everyitem.addEventListener('mouseover', function (e) {
 
-                let el_link = this.querySelector('.dropdown-toggle');
+                    let el_link = this.querySelector('.dropdown-toggle');
 
-                if (el_link != null) {
-                    let nextEl = el_link.nextElementSibling;
-                    el_link.classList.add('show');
-                    nextEl.classList.add('show');
-                }
+                    if (el_link != null) {
+                        let nextEl = el_link.nextElementSibling;
+                        el_link.classList.add('show');
+                        nextEl.classList.add('show');
+                    }
 
+                });
+                everyitem.addEventListener('mouseleave', function (e) {
+                    let el_link = this.querySelector('.dropdown-toggle');
+
+                    if (el_link != null) {
+                        let nextEl = el_link.nextElementSibling;
+                        el_link.classList.remove('show');
+                        nextEl.classList.remove('show');
+                    }
+                })
             });
-            everyitem.addEventListener('mouseleave', function (e) {
-                let el_link = this.querySelector('.dropdown-toggle');
 
-                if (el_link != null) {
-                    let nextEl = el_link.nextElementSibling;
-                    el_link.classList.remove('show');
-                    nextEl.classList.remove('show');
-                }
-            })
-        });
-
-        $('.mobile-only-dropdown').removeClass('dropdown-menu');
-    } //end innerWidth
+            $('.mobile-only-dropdown').removeClass('dropdown-menu');
+        } //end innerWidth
+    }
+    
+    dropdownOnHover();
+    $(window).resize(function () {
+        dropdownOnHover();
+    });
 
 }); //end ready
